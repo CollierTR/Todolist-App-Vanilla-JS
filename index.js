55203da7ca57ba27ce341eb585af91a423f7a437
CollierTR <80648892+CollierTR@users.noreply.github.com>
2024-12-30 22:38:29 -0600

    Added to JS

---
+++ index.js
@@ -0,0 +1,26 @@
+
+let taskArr = [
+    {
+        name: "Feed the Fish",
+        completed: false,
+    },
+    {
+        name: "Read 1st chapter of 'Clean Code'",
+        completed: false,
+    },
+    {
+        name: "Clean Toliet",
+        completed: false,
+    },
+    {
+        name: "Walk the Dog",
+        completed: false,
+    },
+]
+
+
+const taskList = document.getElementById('taskList')
+
+for (task in taskArr) {
+    taskList.innerHTML += task.name
+}
