60cb664c633ea8dadf1bb5adbe8f504ad6f52f77
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-01-06 21:33:32 -0600

    fixed file structure

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+let taskArr = [
+    {
+        name: "Feed the Fish",
+        completed: false,
+    },
+    {
+        name: "Read 1st chapter of 'Clean Code'",
+        completed: false,
+    },
+    {
+        name: "Clean Toliet",
+        completed: false,
+    },
+    {
+        name: "Walk the Dog",
+        completed: false,
+    },
+];
+const taskList = document.getElementById('taskList');
+taskArr.forEach((task) => {
+    taskList.innerHTML += `<div class="taskItem"><p>${task.name}</p></div>`;
+});
