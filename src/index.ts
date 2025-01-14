e25045eb2f2a887858fb041517bc8bd4f84d8e19
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-01-13 21:47:06 -0600

    added a TodoList class to TS file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,57 @@
-
 let taskArr = [
     {
-        name: "Feed the Fish",
+        id: 1,
+        title: "Feed the Fish",
         completed: false,
     },
     {
-        name: "Read 1st chapter of 'Clean Code'",
+        id: 2,
+        title: "Read 1st chapter of 'Clean Code'",
         completed: false,
     },
     {
-        name: "Clean the Toliet",
+        id: 3,
+        title: "Clean the Toliet",
         completed: false,
     },
     {
-        name: "Walk the Dog",
+        id: 4,
+        title: "Walk the Dog",
         completed: false,
     },
 ]
+
+
+class TodoList {
+
+    private todoArray: {id: number, title: string, completed: boolean}[]
+
+    public constructor(array: {id: number, title: string, completed: boolean}[]) {
+        this.todoArray = array;
+    }
+
+    public returnNewId(): number {
+        const maxId = this.todoArray.reduce((max, task) => Math.max(max, task.id), 0);
+        return maxId + 1;
+    }
+
+    public addTask(title: string = "Task") {
+        const newTask = {
+            id: this.returnNewId(),
+            title: title,
+            completed: false,
+        };
+        this.todoArray.push(newTask);
+    }
+
+}
+
+const mainTodoList = new TodoList(taskArr)
+
 
 const taskList = document.getElementById('taskList')
 
 
 taskList && taskArr.forEach((task) => {
-    taskList.innerHTML += `<div class="taskItem"><p>${task.name}</p></div>`
+    taskList.innerHTML += `<div class="taskItem"><p>${task.title}</p></div>`
 }) 
